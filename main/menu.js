6a2652d6d9a0e9a448d773857134913f9e4c5035
katerinatsymbal-del <katerina.tsymbal@gmail.com>
2026-05-29 16:08:06 +0300

    Add files via upload
    initial commit

---
+++ main/menu.js
@@ -0,0 +1,12 @@
+document.getElementById('siteMenu').innerHTML = `
+ <ul>
+    <li class="menu_item"><a href="antonii_rimlyanin.html">АНТОНИЙ РИМЛЯНИН</a></li>
+    <li class="menu_item"><a href="#">Пункт номер два</a></li>
+    <li class="menu_item"><a href="#">Пункт номер три</a></li>
+    <li class="menu_item"><a href="#">Иван Иванов</a></li>
+    <li class="menu_item"><a href="#">Константин Константинопольский</a></li>
+    <li class="menu_item"><a href="#">Валентина Христовоздвиженская</a></li>
+    <li class="menu_item"><a href="#">Юн Ян</a></li>
+    <li class="menu_item"><a href="#">Алексей Алиев</a></li>
+</ul>
+`
